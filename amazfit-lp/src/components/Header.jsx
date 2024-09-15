2e60f195715dbc562beaebf832908bdfc93cdb2f
Johan Vergara <avjohan@uninorte.edu.co>
2024-09-15 16:58:07 -0500

    fix: add tailwind file

--- amazfit-lp/src/components/Header.jsx
+++ amazfit-lp/src/components/Header.jsx
@@ -1,7 +1,17 @@
 export default function Header() {
     return (
       <header className="bg-gray-800 text-white p-4">
-        <h1 className="text-2xl">Amazfit Store</h1>
+        <div className="flex">
+        <img src="/logo.png" alt="" />
+        <nav>
+            <ul className="flex justify-center space-x-4">
+                <li><a href="#intro">Introducción</a></li>
+                <li><a href="#dofa">DOFA</a></li>
+                <li><a href="#organizational-identity">Identidad Organizacional</a></li>
+                <li><a href="#strategic-objectives">Objetivos Estratégicos</a></li>
+            </ul>
+        </nav>
+        </div>
       </header>
     );
   }
