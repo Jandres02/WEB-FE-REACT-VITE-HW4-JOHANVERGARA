351b742940b2e0f3d1386a9d6243382b29b5c768
Johan Vergara <avjohan@uninorte.edu.co>
2024-09-16 08:56:04 -0500

    fix: change paths and config scripts

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/Jandres02/WEB-FE-REACT-VITE-HW4-JOHANVERGARA"
+  base: "https://Jandres02.github.io/WEB-FE-REACT-VITE-HW4-JOHANVERGARA"
 })
